59e6f710a3d054e92d92479af7ff555165d2f09e
James Odell <jpodell21@gmail.com>
2021-11-18 12:42:53 -0700

    working on variables

---
+++ app/variables.py
@@ -0,0 +1,8 @@
+character_name = 'Bob'
+character_hobby = 'skateboard'
+character_age = '10'
+character_dream = 'skate'
+print('There once was a man named ' + character_name + ", ")
+print('He loved to ride his ' + character_hobby + ', ')
+print('He has been riding for over ' + character_age + ' years, ')
+print('It is all he could do because he loved to ' + character_dream + ', ')
