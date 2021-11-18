f319ff54bc8f75f812a71f9d39a7b78f0deee5f6
James Odell <jpodell21@gmail.com>
2021-11-18 12:46:17 -0700

    working on variables

--- app/variables.py
+++ app/variables.py
@@ -4,5 +4,9 @@ character_age = '10'
 character_dream = 'skate'
 print('There once was a man named ' + character_name + ", ")
 print('He loved to ride his ' + character_hobby + ', ')
+
+#giving the variable character_name a new value
+character_name = 'Bill'
+print('He really liked the name ' + character_name + ', ')
 print('He has been riding for over ' + character_age + ' years, ')
 print('It is all he could do because he loved to ' + character_dream + ', ')
